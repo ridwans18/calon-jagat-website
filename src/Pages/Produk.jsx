d194e4895779096a5b16ec500430d360a1bd68ac
abel armando <abelarmando79@gmail.com>
2025-05-13 12:18:36 +0700

    menambahkan halaman produk

--- src/Pages/Produk.jsx
+++ src/Pages/Produk.jsx
@@ -1,12 +1,13 @@
-import SideBar from '../components/Layouts/SideBar';
+import Produk_Layout from "../components/Layouts/Produk_Layout";
+import SideBar from "../components/Layouts/SideBar";
 
 function Produk() {
   return (
     <div className="flex min-h-screen bg-white">
       <SideBar />
       <main className="flex-1 p-6">
-        <h2 className="text-xl font-semibold mb-4">Daftar Produk</h2>
         {/* Add your content here */}
+        <Produk_Layout />
       </main>
     </div>
   );
