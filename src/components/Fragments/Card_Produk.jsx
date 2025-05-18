4cfce7ea8849ad76e8dc1e2b1299f7df51193b00
abel armando <abelarmando79@gmail.com>
2025-05-18 13:05:39 +0700

    manambahkan fetching data

--- src/components/Fragments/Card_Produk.jsx
+++ src/components/Fragments/Card_Produk.jsx
@@ -1,14 +1,18 @@
 import { IoIosArrowDown } from "react-icons/io";
 import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { deletedata, fetchData } from "../../services/api";
 
 function Card_Produk() {
   const [produk, setProduk] = useState([]);
   const navigate = useNavigate();
-  
+  const { data: products, loading, error } = useFetch(() => fetchData("ayam"));
+  console.log("products =>", products);
+  console.log("length =>", products.length);
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem("produk")) || [];
-    setProduk(data);
+    // const data = JSON.parse(localStorage.getItem("produk")) || [];
+    // setProduk(data);
   }, []);
 
   const handleSave = (index, updatedData) => {
@@ -18,10 +22,9 @@ function Card_Produk() {
     localStorage.setItem("produk", JSON.stringify(newProduk));
   };
 
-  const handleDelete = (indexToDelete) => {
-    const newProduk = produk.filter((_, index) => index !== indexToDelete);
-    setProduk(newProduk);
-    localStorage.setItem("produk", JSON.stringify(newProduk));
+  const handleDelete = async (indexToDelete) => {
+    await deletedata("ayam", indexToDelete);
+    console.log("terhapus");
   };
 
   const [dropdownIndex, setDropdownIndex] = useState(null);
@@ -32,17 +35,17 @@ function Card_Produk() {
   };
 
   useEffect(() => {
-  const handleClickOutside = (event) => {
-    if (
-      dropdownIndex !== null &&
-      dropdownRefs.current[dropdownIndex] &&
-      !dropdownRefs.current[dropdownIndex].contains(event.target)
-    ) {
-      setDropdownIndex(null);
-    }
-  };
+    const handleClickOutside = (event) => {
+      if (
+        dropdownIndex !== null &&
+        dropdownRefs.current[dropdownIndex] &&
+        !dropdownRefs.current[dropdownIndex].contains(event.target)
+      ) {
+        setDropdownIndex(null);
+      }
+    };
 
-      document.addEventListener("click", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
@@ -50,97 +53,92 @@ function Card_Produk() {
 
   return (
     <div className="flex flex-col gap-2 ">
-      {produk.length === 0 ? (
-      <p>Tidak ada produk</p>
+      {products.length === 0 ? (
+        <p>Tidak ada produk</p>
       ) : (
-        (produk || []).map((item, index) => (
-        <div 
-          key={index}
-          className="flex gap-4 items-start p-4 border rounded-lg shadow-sm bg-white"
-        >
+        (products || []).map((item, index) => (
+          <div
+            key={index}
+            className="flex gap-4 items-start p-4 border rounded-lg shadow-sm bg-white"
+          >
+            {/* Checkbox */}
+            <input type="checkbox" className="w-3 h-3" />
 
-          {/* Checkbox */}
-          <input type="checkbox" className="w-3 h-3" />
-
-          {/* Gambar dan Nama Produk */}
-          <div className="min-w-[323px] flex gap-2">
-            {item.images?.map((src, i) => (
-            <img 
-              key={i}
-              src={src} 
-              alt={`produk-${i}`} 
-              className="w-20 h-20 object-cover border rounded" 
-            />
-            ))}
-            <p>{item.nama}</p>
-          </div>
-
-          {/* Harga */}
-          <div className="min-w-[150px] w-full">
-            <div className="flex h-[40px] w-[150px] items-center gap-2  rounded-md overflow-hidden border-secondary border">
-              <div className="bg-secondary h-full flex items-center justify-center  min-w-[30px]">
-                <p>Rp</p>
-              </div>
-              <input
-                type="text"
-                value={item.harga}
-                className="w-full outline-none"
-                readOnly
-              />
+            {/* Gambar dan Nama Produk */}
+            <div className="min-w-[323px] flex gap-2">
+              {item.images?.map((src, i) => (
+                <img
+                  key={i}
+                  src={src}
+                  alt={`produk-${i}`}
+                  className="w-20 h-20 object-cover border rounded"
+                />
+              ))}
+              <p>{item.nama}</p>
             </div>
-          </div>
 
-          {/* Stok */}
-          <div className="min-w-[160px] w-full">
-            <input 
-              type="text" 
-              value={item.stok}
-              readOnly
-            />
-          </div>
-
-          {/* Deskripsi */}
-          <div className="min-w-[150px] w-full">
-            <p>{item.deskripsi}</p>
-          </div>
-
-          {/* Status */}
-          <div className="min-w-[150px] w-full">
-            <p>Tersedia</p>
-          </div>
-
-          {/* Opsi Dropdown */}
-          <div className="min-w-[150px] w-full">
-            {/* Tombol Hapus */}
-            <div className="relative">
-              <div
-                onClick={() => toggleDropdown(index)}
-                ref={(el) => (dropdownRefs.current[index] = el)}
-                className="flex items-center justify-between p-2 border-2 border-secondary rounded-md cursor-pointer hover:bg-green-100"
-              >
-                <p>Atur</p>
-                <IoIosArrowDown />
-              </div>
-
-              {dropdownIndex === index && (
-                <div className="absolute top-full w-39 bg-white border border-gray-50 shadow-md rounded-md text-sm">
-                  <button
-                    onClick={() => (handleDelete(index))}
-                    className="w-full text-left px-4 py-2 hover:bg-red-100 text-red-600 rounded-md"
-                  >
-                    Hapus
-                  </button>
-                  <button
-                    onClick={() => navigate("/EditProduk")}
-                    className="w-full text-left px-4 py-2 hover:bg-blue-100 text-blue-600 rounded-md"
-                  >
-                    Edit
-                  </button>
+            {/* Harga */}
+            <div className="min-w-[150px] w-full">
+              <div className="flex h-[40px] w-[150px] items-center gap-2  rounded-md overflow-hidden border-secondary border">
+                <div className="bg-secondary h-full flex items-center justify-center  min-w-[30px]">
+                  <p>Rp</p>
                 </div>
-              )}
+                <input
+                  type="text"
+                  value={item.price}
+                  className="w-full outline-none"
+                  readOnly
+                />
+              </div>
+            </div>
+
+            {/* Stok */}
+            <div className="min-w-[160px] w-full">
+              <input type="text" value={item.stok} readOnly />
+            </div>
+
+            {/* Deskripsi */}
+            <div className="min-w-[150px] w-full">
+              <p>{item.deskripsi}</p>
+            </div>
+
+            {/* Status */}
+            <div className="min-w-[150px] w-full">
+              <p>Tersedia</p>
+            </div>
+
+            {/* Opsi Dropdown */}
+            <div className="min-w-[150px] w-full">
+              {/* Tombol Hapus */}
+              <div className="relative">
+                <div
+                  onClick={() => toggleDropdown(index)}
+                  ref={(el) => (dropdownRefs.current[index] = el)}
+                  className="flex items-center justify-between p-2 border-2 border-secondary rounded-md cursor-pointer hover:bg-green-100"
+                >
+                  <p>Atur</p>
+                  <IoIosArrowDown />
+                </div>
+
+                {dropdownIndex === index && (
+                  <div className="absolute top-full w-39 bg-white border border-gray-50 shadow-md rounded-md text-sm z-90">
+                    <button
+                      onClick={() => handleDelete(item.id)}
+                      className="w-full text-left px-4 py-2 hover:bg-red-100 text-red-600 rounded-md"
+                    >
+                      Hapus
+                    </button>
+                    <button
+                      onClick={() => navigate("/EditProduk")}
+                      className="w-full text-left px-4 py-2 hover:bg-blue-100 text-blue-600 rounded-md"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
         ))
       )}
     </div>
