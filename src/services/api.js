5aabb8a63b9e28b16e41bfdcd373532384ca63a4
abel armando <abelarmando79@gmail.com>
2025-05-18 13:07:33 +0700

    menambahkan fetching delete dan edit

--- src/services/api.js
+++ src/services/api.js
@@ -11,11 +11,36 @@ export const fetchData = async (url) => {
   }
 };
 
-export const posdata = async (url, data) => {
+export const postdata = async (url, isidata) => {
   try {
     const response = await axios.post(
       `${import.meta.env.VITE_API_baseurl}${url}`,
-      data
+      isidata
+    );
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletedata = async (url, id) => {
+  try {
+    const response = await axios.delete(
+      `${import.meta.env.VITE_API_baseurl}${url}/${id}`
+    );
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updatedata = async (url, id, isidata) => {
+  try {
+    const response = await axios.put(
+      `${import.meta.env.VITE_API_baseurl}${url}/${id}`,
+      isidata
     );
     const data = await response.data;
     return data;
