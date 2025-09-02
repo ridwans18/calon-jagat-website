eac90c9154c5fc691016388b6d7192b6afab0ad8
abel armando <abelarmando79@gmail.com>
2025-09-02 13:18:08 +0700

    menambahkan storage management dan membuat function untuk search dan filtering datra

--- src/components/Fragments/ResetFilter.jsx
+++ src/components/Fragments/ResetFilter.jsx
@@ -1,9 +1,12 @@
-function ResetFilter() {
-    return (
-    <button className="text-green-600 text-sm font-semibold hover:underline w-18">
-        Reset Filter
+function ResetFilter({ fungsi }) {
+  return (
+    <button
+      className="text-green-600 text-sm font-semibold hover:underline w-18"
+      onClick={() => fungsi()}
+    >
+      Reset Filter
     </button>
-    );
+  );
 }
 
 export default ResetFilter;
--- src/components/Layouts/Filter-Search-Reset.jsx
+++ src/components/Layouts/Filter-Search-Reset.jsx
@@ -1,30 +1,72 @@
 import Dropdown from "../Fragments/Dropdown";
 import SearchBar from "../Fragments/SearchBar";
 import ResetFilter from "../Fragments/ResetFilter";
+import useDataOrders from "../../../store";
+import { useShallow } from "zustand/shallow";
+import { useEffect, useState } from "react";
 
 function FilterSearchReset() {
+  const [test, settest] = useState("");
+  const {
+    search,
+    SearchInput,
+    DateAsc,
+    DateDesc,
+    SearchName,
+    ResetData,
+    ResetSearch,
+  } = useDataOrders(
+    useShallow((state) => ({
+      ResetSearch: state.ResetSearch,
+      ResetData: state.ResetData,
+      search: state.search,
+      SearchInput: state.SearchInput,
+      DateAsc: state.DateAsc,
+      DateDesc: state.DateDesc,
+      SearchName: state.SearchName,
+    }))
+  );
+  const reset = (a) => {
+    console.log(a);
+  };
+  useEffect(() => {
+    SearchName();
+    if (search === "") ResetData();
+  }, [search]);
+
   return (
     <div className="mt-6 bg-gray-100 border border-gray-200 rounded-md shadow-sm overflow-hidden p-4 ">
       {/* Filter & Search Section */}
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
         {/* Kiri: Dropdown + Checkbox */}
-        <Dropdown>
-          <option>Pilih Filter</option>
-          <option>Paling Baru</option>
-          <option>Paling Lama</option>
-        </Dropdown>
+        <select
+          onChange={(e) => {
+            if (e.target.value === "asc") {
+              console.log("Filter Asc jalan");
+              DateDesc();
+            } else if (e.target.value === "desc") {
+              console.log("Filter DESC jalan");
+
+              DateAsc();
+            }
+          }}
+          className="bg-white border border-gray-300 rounded px-3 py-2 text-sm text-gray-700"
+        >
+          <option value="asc">Paling Baru</option>
+          <option value="desc">Paling Lama</option>
+        </select>
 
         <div className="w-full">
-          <button
-            className="text-sm text-gray-700 px-5 h-10 rounded-md border border-gray-200 hover:bg-green-100 bg-white">
-              Cetak
+          <button className="text-sm text-gray-700 px-5 h-10 rounded-md border border-gray-200 hover:bg-green-100 bg-white">
+            Cetak
           </button>
         </div>
 
         {/* Kanan: Search + Reset */}
         <div className="flex items-center gap-4">
-          <SearchBar />
-          <ResetFilter />
+          <SearchBar search={search} setSearch={SearchInput} />
+          <ResetFilter fungsi={ResetSearch} />
+          {/* <button onClick={() => reset()}>aa</button> */}
         </div>
       </div>
     </div>
